b6d7c26621e2ea4f1f7d15d783f28c76379b9c98
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-07-09 23:57:17 -0400

    AJAX

--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,23 +1,37 @@
-const charactersAPI = new APIHandler("http://ih-api.herokuapp.com")
+const charactersAPI = new APIHandler("http://ih-api.herokuapp.com");
 
 $(document).ready( () => {
   $('#fetch-all').on('click', (e) => {
+    event.preventDefault();
+    const characterInfo = {
+      name: $('#the-name-input').val(),
+      occupation: $('#the-occupation-input').val(),
+      weapon: $('#the-weapon-input').val()
+    };
 
-  })
+    $.ajax({
+      method: 'GET',
+      url: 'https://ih-api.herokuapp.com/characters',
+      data: characterInfo,
+      success: showFeedback,
+      error: handleError
+    });
+  });
+});
 
   $('#fetch-one').on('click', (e) => {
 
-  })
+  });
 
   $('#delete-one').on('click', (e) => {
 
-  })
+  });
 
   $('#edit-character-form').on('submit', (e) => {
 
-  })
+  });
 
   $('#new-character-form').on('submit', (e) => {
 
-  })
-})
+  });
+});
